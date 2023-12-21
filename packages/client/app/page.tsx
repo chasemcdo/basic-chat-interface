dfe3b62498c356b05c8ec851c508fa3fe3e94984
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-21 15:42:00 -0800

    add send and add button with fixed margins

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -14,9 +14,19 @@ export default function Home() {
             <button className='w-[149px] h-[40px] border border-gray-400 text-gray-400 rounded-full'>Task</button>
             <button className='w-[149px] h-[40px] border border-gray-400 text-gray-400 rounded-full'>Task</button>
           </div>
-          <div className='my-[16px] px-[32px] py-4 flex flex-row w-full text-gray-900 border border-gray-300 rounded-lg sm:text-md'>
+          <div className='my-[16px] px-[18px] py-4 items-center flex flex-row w-full text-gray-900 border border-gray-300 rounded-xl sm:text-md'>
             {/* Message Input */}
-            <input placeholder="What's on your mind?" type="text" className="focus:outline-none" />
+            <button className="px-[4px] text-gray-400">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[30px] h-[30px]">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+            </button>
+            <input placeholder="What's on your mind?" type="text" className="focus:outline-none w-full" />
+            <div className="text-gray-400">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[30px] h-[30px]">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
