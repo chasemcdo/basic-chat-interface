b0a6b5788ed7c6c7de60c7d1ab709c6e18022d35
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-23 18:25:40 -0800

    Check for message param

--- packages/api/routes/chats.ts
+++ packages/api/routes/chats.ts
@@ -2,7 +2,16 @@ import { NextFunction, Request, Response, Router } from "express";
 
 var router = Router();
 
+// Send Message
 router.post('/', function(req: Request, res: Response, next: NextFunction) {
+  // Check if message param exists
+  const message = req.body.message
+  if (!message) {
+    return res.status(400).send({
+      message: 'The message param is required'
+    });
+  }
+
   res.send({
     message: 'Message Sent Successfully!'
   });
