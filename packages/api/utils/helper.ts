af3d9bd0b2db4b2f7153a0295b6810e766441e2a
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-23 22:14:32 -0800

    prettier format

--- packages/api/utils/helper.ts
+++ packages/api/utils/helper.ts
@@ -1,12 +1,12 @@
-import User from '../models/user'
+import User from "../models/user";
 
 export const getDefaultChatId = async (): Promise<string> => {
-  return await User.findByUsername('defaultuser', false).then((user) => {
+  return await User.findByUsername("defaultuser", false).then((user) => {
     if (user !== null) {
-      return user.chatId
+      return user.chatId;
     } else {
-      Error('Default user does not exist')
-      return null
+      Error("Default user does not exist");
+      return null;
     }
-  })
-}
+  });
+};
