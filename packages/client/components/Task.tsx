29613391b7150757cf3371449bcebb7356b16c6d
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-27 19:20:37 -0800

    fix lint issues

--- packages/client/components/Task.tsx
+++ packages/client/components/Task.tsx
@@ -3,7 +3,7 @@ type Props = {
   action: () => Promise<void>;
 };
 
-export default ({title, action}: Props) => {
+const Task = ({title, action}: Props) => {
   return (
     <button 
       className='max-w-[149px] w-full h-[40px] border border-gray-400 text-gray-400 rounded-full hover:border-gray-900 hover:text-gray-900 hover:bg-gray-100'
@@ -13,3 +13,5 @@ export default ({title, action}: Props) => {
     </button>
   );
 };
+
+export default Task;
