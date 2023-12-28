a3a7d79911826907ec5546d8a74af51ba22bc50c
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-27 19:13:24 -0800

    add hover effect

--- packages/client/components/Task.tsx
+++ packages/client/components/Task.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default ({title, action}: Props) => {
   return (
     <button 
-      className='w-[149px] h-[40px] border border-gray-400 text-gray-400 rounded-full'
+      className='w-[149px] h-[40px] border border-gray-400 text-gray-400 rounded-full hover:border-gray-900 hover:text-gray-900 hover:bg-gray-100'
       onClick={action}
     >
       {title}
