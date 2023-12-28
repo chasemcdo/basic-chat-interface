f3d59166f55f31f359f50b1050448c7dfd6907b8
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-27 19:51:22 -0800

    add no message indicator

--- packages/client/components/Messages.tsx
+++ packages/client/components/Messages.tsx
@@ -28,19 +28,27 @@ const Messages = ({chatHistory, loading}: Props) => {
       </div>
     )
   } else {
-    return (
-      <div className='flex flex-col h-full'>
-        {chatHistory.map((message, index) => (
-          <div key={index} className='flex flex-row items-center'>
-            <div className='w-[30px] h-[30px] rounded-full bg-gray-400'></div>
-            <div className='flex flex-col ml-[10px]'>
-              <span className='text-gray-900 font-bold'>{message.type}</span>
-              <span className='text-gray-500'>{message.message}</span>
-            </div>
-          </div>
-        ))}
+    if (chatHistory.length === 0) {
+      return (
+      <div className='flex flex-col h-full items-center justify-center'>
+        <span className='text-gray-400'>No messages yet.</span>
       </div>
-    )
+      )
+    } else {
+      return (
+        <div className='flex flex-col h-full'>
+          {chatHistory.map((message, index) => (
+            <div key={index} className='flex flex-row items-center'>
+              <div className='w-[30px] h-[30px] rounded-full bg-gray-400'></div>
+              <div className='flex flex-col ml-[10px]'>
+                <span className='text-gray-900 font-bold'>{message.type}</span>
+                <span className='text-gray-500'>{message.message}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      )
+    }
   }
 };
 
