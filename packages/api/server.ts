432d5559cd49057d2083b511d15a048ad592ada3
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-28 16:34:42 -0800

    move mongoose connection from app to server

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -1,5 +1,27 @@
+import mongoose from "mongoose";
 import app from "./app";
 import env from "./utils/env";
+import User from "./models/user";
+import { ObjectId } from "mongodb";
+
+// Connect to MongoDB
+mongoose.connect(env.MONGODB_URI).then(() => {
+  // Create default user if it doesn't exist
+  User.findByUsername('defaultuser', false).then((user) => {
+    if (user) {
+      console.log('Default user already exists');
+      return;
+    } else {
+      new User({
+        username: 'defaultuser',
+        firstName: 'John',
+        lastName: 'Doe',
+        chatId: new ObjectId().toString(),
+      }).save();
+      console.log('Default user created');
+    }
+  })
+});
 
 app.listen(env.PORT, () => {
   console.log("Service is listening on port 5678");
