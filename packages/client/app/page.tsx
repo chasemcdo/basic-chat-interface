62386030c506889aaf32c44704725457042f7d57
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-29 11:17:03 -0800

    keep toggleable search button

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -69,24 +69,22 @@ export default function Home() {
         <Sidebar open={sidebarOpen} />
         <div className="flex flex-row relative w-full h-full bg-white">
           <div className="absolute right-0 top-0 m-4 flex flex-col gap-2">
-            {!searchVisible && (
-              <button onClick={() => setSearchVisible(true)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                  />
-                </svg>
-              </button>
-            )}
+            <button onClick={() => setSearchVisible(!searchVisible)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </button>
             <button onClick={resetChat}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
