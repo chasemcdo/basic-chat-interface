825bc314d676f99d0c5078ea3c81da95eca9d97f
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-29 00:21:18 -0800

    update tests to add full page checking

--- packages/client/__tests__/chat.test.tsx
+++ packages/client/__tests__/chat.test.tsx
@@ -1,14 +1,44 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
 import React from 'react'
 import Page from '../app/page'
- 
-describe('Page', () => {
-  it('renders a heading', () => {
+import ReactDOM from 'react-dom/client';
+import fetchMock from 'jest-fetch-mock';
+import Messages from '@/components/Messages';
+import { ChatMessage } from '@/helpers/types';
+import { render, screen, waitFor } from '@testing-library/react';
+
+fetchMock.enableMocks()
+
+const messages: ChatMessage[] = [
+  {
+    message: "Hello ai",
+    type: "human"
+  },
+  {
+    message: "Hello human",
+    type: "ai"
+  }
+]
+
+beforeEach(() => {
+  fetchMock.mockIf(/^.*$/, async _req => {
+    return { body: JSON.stringify(messages)}
+  })
+})
+
+describe('Messages Component', () => {
+  it('renders', () => {
+    render(<Messages 
+      chatHistory={messages}
+      loading={false}
+    />)
+
+    expect(screen.getByText('Hello ai'))
+  })
+})
+
+describe('Chat Interface', () => {
+  it('renders messages', async () => {
     render(<Page />)
- 
-    // const heading = screen.getByRole('heading', { level: 1 })
- 
-    // expect(heading).toBeInTheDocument()
+    await waitFor(() => screen.getByText('Hello human'))
   })
 })
