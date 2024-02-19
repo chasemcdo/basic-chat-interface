1270d5a8fb0d21a070dd19887f141b7156e49533
Chase McDougall <chasemcdougall@hotmail.com>
2024-02-19 16:12:07 -0500

    Update generate_appointments.py

--- packages/api-python/app/generate_appointments.py
+++ packages/api-python/app/generate_appointments.py
@@ -2,7 +2,14 @@ import datetime
 import random
 import json
 
-def generate_appointments(start_date, days=7, appointments_per_day=3):
+def generate_appointments(start_date: datetime, days: int = 7, appointments_per_day: int = 3):
+    """Generate a sequence of available appointments for a given date range which is stored in a JSON file.
+
+    Args:
+        start_date: First integer
+        days: Days to generate appointments for
+        appointments_per_day: Appointments per day
+    """
     appointments = []
     for day in range(days):
         date = start_date + datetime.timedelta(days=day)
@@ -22,8 +29,9 @@ def generate_appointments(start_date, days=7, appointments_per_day=3):
     file_path = "appointments.json"
     with open(file_path, 'r') as file:
         data = json.load(file)
+
+    appointments = data + appointments
     with open(file_path, "w") as file:
         json.dump(appointments, file, indent=4)
 
-    appointments = data + appointments
     return appointments
